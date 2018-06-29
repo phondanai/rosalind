59ef2985a99bda0ddc1841d5bfc583b86ed12fbd
Phondanai Khanti <phondanai@gmail.com>
2018-06-29 16:51:16 +0700

    Add Introduction_to_Protein_Databases solution

---
+++ Introduction_to_Protein_Databases/Introduction_to_Protein_Databases.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+
+
+with open('rosalind_dbpr.txt', 'r') as f:
+    uniprot_id = f.read().strip()
+
+resp = urlopen('http://www.uniprot.org/uniprot/{}.txt'.format(uniprot_id))
+
+uniprot_text = resp.read().decode('utf-8')
+uniprot_list = uniprot_text.split('\n')
+
+dna_list = [i for i in uniprot_list if "GO;" in i ]
+
+#protein_result = [i[i.index('DNA'):i.index(';', i.index('DNA'))] for i in dna_list ]
+protein_result = [i.split(';')[2].strip()[2:] for i in dna_list if 'P:' in i]
+
+for i in protein_result:
+    print(i)
+
